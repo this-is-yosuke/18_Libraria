29be3878adfce1db70dfc0b24638b8c761d57861
this-is-yosuke <yosuke.kibe@gmail.com>
2025-03-09 06:30:51 -0600

    Updated server
    Fixed the paths used when the app is in production

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,10 +35,10 @@ const startApolloServer = async () => {
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../../client/build')));
   
   app.get('*', (_req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
 
